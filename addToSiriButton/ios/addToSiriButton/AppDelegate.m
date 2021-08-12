e3b26cd152506ce38d39c6376c11eac37ba8803e
Ger Vang <=>
2021-08-12 14:49:46 -0500

    Add to Siri Button

--- addToSiriButton/ios/addToSiriButton/AppDelegate.m
+++ addToSiriButton/ios/addToSiriButton/AppDelegate.m
@@ -11,43 +11,6 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-@import RNSiriShortcuts;
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  ...
-
-  // Check if the app launched with any shortcuts
-  BOOL launchedFromShortcut = [launchOptions objectForKey:@"UIApplicationLaunchOptionsUserActivityDictionaryKey"] != nil;
-  // Add a boolean to the initialProperties to let the app know you got the initial shortcut
-  NSDictionary *initialProperties = @{ @"launchedFromShortcut":@(launchedFromShortcut) };
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"example"
-                                            initialProperties:initialProperties]; // Add the initial properties here
-
-  ...
-}
-
-// This method checks for shortcuts issued to the app
-- (BOOL)application:(UIApplication *)application
-continueUserActivity:(NSUserActivity *)userActivity
- restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> *restorableObjects))restorationHandler
-{
-  UIViewController *viewController = [self.window rootViewController];
-  RCTRootView *rootView = (RCTRootView*) [viewController view];
-
-  // If the initial properties say the app launched from a shortcut (see above), tell the library about it.
-  if ([[rootView.appProperties objectForKey:@"launchedFromShortcut"] boolValue]) {
-    ShortcutsModule.initialUserActivity = userActivity;
-
-    rootView.appProperties = @{ @"launchedFromShortcut":@NO };
-  }
-
-  [ShortcutsModule onShortcutReceivedWithUserActivity:userActivity];
-
-  return YES;
-}
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
